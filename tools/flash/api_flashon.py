5a03bb59098cb99dadc1d6f0012b177cb38e847c
CaiqueONZZ <151483729+CaiqueONZZ@users.noreply.github.com>
2023-12-11 21:29:01 -0300

    Delete tools directory

--- tools/flash/api_flashon.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-        install_requires=['setuptools','poetry','functools'],
-        name="flash_api",
-        zip_safe=False,
-        version=1.0
-        )
