2087427a011bda5ba71df0e3663d1d62d3816ce6
CaiqueONZZ <151483729+CaiqueONZZ@users.noreply.github.com>
2023-12-11 21:32:31 -0300

    Add files via upload

---
+++ tools/flash/api_flashon.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+        install_requires=['setuptools','poetry','functools'],
+        name="flash_api",
+        zip_safe=False,
+        version=1.0
+        )
